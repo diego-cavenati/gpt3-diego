d4a7c50c0f0d061f9ef4a0fee684051271cb8363
diego-cavenati <diegocavenati@gmail.com>
2023-03-19 20:20:53 +0100

    header

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import './header.css';
 
-
 const Header = () => {
     return (
-        <div>Header</div>
+        <div className='gpt3__header section__padding' id='home'>
+            <div className='gpt3_header-content'>
+                <h1 className='gradient__text'>Let's Build Something amazing with GPT-3 OpenAI</h1>
+            </div>
+        </div>
     )
 }
 
